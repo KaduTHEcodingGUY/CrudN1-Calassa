2ac213876ff0c1142b11b790f6291a0a34752a8d
KaduTHEcodingGUY <megatrow12@gmail.com>
2025-09-25 15:09:08 -0300

    Add files via upload

---
+++ CrudN1/CrudN1/src/test/java/com/example/CrudN1/CrudN1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrudN1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudN1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
